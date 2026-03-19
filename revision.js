8da918e641ef13d9d8652326fea74010cedec70d
chinmayd1 <chinmay.minskole@gmail.com>
2026-03-19 21:47:04 +0530

    added

---
+++ revision.js
@@ -0,0 +1,74 @@
+
+
+// program 1
+// let a = 10
+// console.log(a)
+// a = 100
+// console.log(a)
+
+// const h = 100
+// console.log(h)
+// //h = 1000
+
+// // Arithmetic operation
+
+// let s = 6
+// let t = 3
+
+// console.log(s+t)
+// console.log(s-t)
+// console.log(s*t)
+// console.log(s/t)
+// console.log(s%t)
+
+// let q = 9
+// let r = 3
+
+// console.log(q+r)
+// console.log(q-r)
+// console.log(q*r)
+// console.log(q/r)
+// console.log(q%r)
+
+// // 1 pair ---> 5 lines 
+// // 10 pair ---> 50 lines of code
+// function Calculator(x,y){
+//     console.log(x+y)
+//     console.log(x-y)
+//     console.log(x*y)
+//     console.log(x/y)
+//     console.log(x%y)
+// }
+// Calculator(12,4)
+// Calculator(6,3)
+
+// Day2 
+
+// function without parameter and without return type
+
+function addA(){
+    console.log(9+9)
+}
+addA()
+addA()
+addA()
+
+// function with parameter and without return type 
+
+function addB(x,y){
+    console.log(x+y)
+}
+addB(12,3)
+addB(12,1)
+
+// function with parameter and with return type
+// 100 given , shown
+
+function addC(x,y){
+    return x + y
+}
+let q2 = addC(12,3)
+console.log(q2)
+console.log(q2 + q2)
+console.log(q2 / 5)
+console.log(q2 * 0.10)
