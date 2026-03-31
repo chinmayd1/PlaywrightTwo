a57be0c9c6dc2f3b45f6457bb731c99b1228ef33
chinmayd1 <chinmay.minskole@gmail.com>
2026-03-31 22:03:41 +0530

    added

---
+++ script9.js
@@ -0,0 +1,46 @@
+// print 1 to 3
+// program 1
+
+for(let i = 1 ; i <= 3 ; i++){ // 2 //3 // 4
+    console.log(i) //1 //2 // 3
+}
+
+//1 <= 3 ---> true
+//2 <= 3 ---> true
+//3 <= 3 ---> true
+//4 <= 3 ---> false
+
+// print 1 to 5
+// program 2
+for(let i = 1 ; i <= 5 ; i = i + 1){ //2 //3 //4 //5 // 6
+    console.log(i) //1 //2 //3 //4 // 5
+}
+// program 3
+// print hello 3 times 
+for(let i = 0 ; i < 3 ;i++){ //1 // 2 // 3
+    console.log("hello") // "hello" "hello" ,"hello"
+}
+
+// program 4
+// print 5 to 1
+for(let i = 5 ; i >= 1 ;i-- ){ // 4 // 3 //2 //1 // 0
+    console.log(i) // 5 // 4 //3 //2 //1
+}
+// program 5
+// print table of 2
+for(let i = 1 ; i <= 10 ; i++){
+    console.log(i * 2)
+}
+
+for(let i = 2 ; i <= 20 ; i = i + 2){ //4 // 6 // 8 --- 22
+    console.log(i) //2 // 4 // 6 ----- 20
+}
+
+// program 6
+// table of 5 in reverse 
+for(let i = 50 ; i >= 5 ; i= i-5){ // 45 // 40 // 35 --- 0
+    console.log(i) // 50 // 45 // 40 ---- 5
+}
+for(let i = 30 ; i >= 3 ; i = i - 3){
+    console.log(i)
+}
