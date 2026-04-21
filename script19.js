f2c0e06d8738a1611c53a27e7943c07777e8d00f
chinmayd1 <chinmay.minskole@gmail.com>
2026-04-21 21:56:57 +0530

    added

---
+++ script19.js
@@ -0,0 +1,70 @@
+
+let firstName = "chinmay"
+console.log(firstName)
+console.log(typeof firstName)
+
+let lastName = 'deshpande'
+console.log(lastName)
+console.log(typeof lastName)
+
+let middleName  = `shirish`
+console.log(middleName)
+console.log(typeof middleName)
+
+
+
+// Student
+// Properties - age , name , gender , weight , height
+// Methods - walk() , talk()
+
+
+
+// methods of string
+// Method -  Gym()
+// action - excercise
+// return - health
+
+let city = "pune"
+let q1 = city.length
+console.log(q1)
+
+let city2 = "nagpur"
+let q2 = city2.toUpperCase() // "NAGPUR"
+console.log(q2)
+
+let city3 = "MUMBAI"
+let q3 = city3.toLowerCase()
+console.log(q3)
+
+// Method chaining
+let q4 = "rahul".toUpperCase().toLowerCase().length
+// "RAHUL"              //"rahul"    5
+console.log(q4)
+
+
+// program 3
+
+let city4 = "wardha"
+let q5 = city4.includes("A")
+console.log(q5)
+let q6 = city4.includes("dha")
+console.log(q6)
+
+
+// program4 
+
+let city6 = "chinmayaya"
+
+// 0   1   2   3   4   5   6  7  8 9
+// c   h   i   n   m   a   y  a  y a 
+
+
+let q7 = city6.indexOf("a")
+console.log(q7)
+
+let q8 = city6.indexOf("a",6)
+console.log(q8)
+
+let q9 = city6.lastIndexOf("a")
+console.log(q9)
+
