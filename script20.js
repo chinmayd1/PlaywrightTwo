6373155bb38a14427a0251563c3aebd87f5cfb02
chinmayd1 <chinmay.minskole@gmail.com>
2026-04-24 22:00:30 +0530

    added

--- script20.js
+++ script20.js
@@ -94,5 +94,59 @@ let q18 = info.replaceAll('js',"python")
 console.log(q18)
 
 // slice()
+// 0  1   2   3   4   5  6   7   8   9
+// c  h   a   n   d   r  a   p   u   r
+//-10 -9 -8  -7  -6  -5 -4  -3  -2  -1
 
+let str2 = "chandrapur"
+// str2.slice(startIndex)
+// str2.slice(startIndex,endIndex(not included))
+let e1  = str2.slice(2)
+console.log(e1)
+console.log(str2.slice(-3))
+console.log(str2.slice(2,8))
+console.log(str2.slice(-9,-1))
+console.log(str2.slice(-9,8))
+console.log(str2.slice(1,-1))
+console.log(str2.slice(-1,-5))
+
+// program 2
 // repeat()
+let str3 = "hi"
+console.log(str3.repeat(3)) // "hihihi"
+
+// program 3
+//chinmay---07111993
+let username= "chinmay"
+let dob = "07111993"
+console.log(`${username}${"-".repeat(3)}${dob}`)
+
+// padStart()
+let numberA = "5" //##5,
+let numberB = "123" // 123
+let numberC  = "89" // #89
+console.log(numberA.padStart(3,"#"))
+console.log(numberB.padStart(3,"#"))
+console.log(numberC.padStart(3,"#"))
+
+// padEnd()
+let numberD = "5" 
+let numberE = "123" 
+let numberF  = "89" 
+numberD.padEnd(3,"#")
+numberE.padEnd(3,"#")
+numberF.padEnd(3,"#")
+
+//contact()
+let str5 = "hello"
+let str6 = "bye"
+console.log(str5.concat(str6))
+console.log(str5.concat(" ").concat(str6))
+
+let info2 = ["chinmay","deshpande",7709192441]
+let w2 = info2.join('@')
+console.log(w2)
+let w3 = w2.split('@') // ["chinmay","deshpande",7709192441]
+console.log(w3)
+
+// Array - 5pm 
